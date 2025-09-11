f7e23a1daece1bbc75b2111b772e0d516ef599a6
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2025-09-11 21:38:31 +0200

    Created base class for all DSP

--- Audio-Plugin-CPP/Source/PluginProcessor.h
+++ Audio-Plugin-CPP/Source/PluginProcessor.h
@@ -64,10 +64,32 @@ public:
         END_OF_LIST
     };
 
+    using DSP_Order = std::array<DSP_Option, static_cast<site_t>(DSP_Option::END_OF_LIST)>;
+
 private:
-    juce::dsp::Phaser<float> phaser;
-    juce::dsp::Chorus<float> chorus;
-    juce::dsp::LadderFilter<float> overdrive, ladderFilter;
+    DSP_Order dspOrder;
+
+    template<typename DSP>
+    struct DSP_Choice : juce::dsp::ProcessorBase {
+        void prepare(const juce::dsp::ProcessSpec& spec) override {
+            dsp.prepare(spec);
+        }
+        void process(const juce::dsp::ProcessContextReplacing<float>& context) override {
+            dsp.process(context);
+        }
+        void reset() override {
+            dsp.reset();
+        }
+
+        DSP dsp;
+    };
+
+    DSP_Choice<juce::dsp::DelayLine<float>> delay;
+    DSP_Choice<juce::dsp::Phaser<float>> phaser;
+    DSP_Choice<juce::dsp::Chorus<float>> chorus;
+    DSP_Choice<juce::dsp::LadderFilter<float>> overdrive, ladderFilter;
+
+    using DSP_Pointers = std::array<juce::dsp::ProcessorBase*, static_cast<site_t>(DSP_Option::END_OF_LIST)>;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginCPPAudioProcessor)
 };
