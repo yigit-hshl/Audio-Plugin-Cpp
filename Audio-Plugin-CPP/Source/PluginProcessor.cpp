3dd64a14b483b575a7eaf5b631f1d2e838fddb55
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2025-09-12 16:16:15 +0200

    Added sorting and processing of dsp pointers

--- Audio-Plugin-CPP/Source/PluginProcessor.cpp
+++ Audio-Plugin-CPP/Source/PluginProcessor.cpp
@@ -144,6 +144,50 @@ void AudioPluginCPPAudioProcessor::processBlock (juce::AudioBuffer<float>& buffe
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
+    auto newDSPOrder = DSP_Order();
+
+    // Try to pull
+    while (dspOrderFifo.pull(newDSPOrder)) {
+
+    }
+
+    // If pulled; replace dspOrder
+    if (newDSPOrder != DSP_Order())
+        dspOrder = newDSPOrder;
+
+    // Convert dspOrder into an array of pointers
+    DSP_Pointers dspPointers;
+
+    for (size_t i = 0; i < dspPointers.size(); ++i) {
+        switch (dspOrder[i]) {
+            case DSP_Option::Phase:
+                dspPointers[i] = &phaser;
+                break;
+            case DSP_Option::Chorus;
+                dspPointers[i] = &chorus;
+                break;
+            case DSP_Option::Overdrive:
+                dspPointers[i] = &overdrive;
+                break;
+            case DSP_Option::LadderFilter;
+                dspPointers[i] = &ladderFilter;
+                break;
+            case DSP_Option::END_OF_LIST;
+                jassertfalse;
+                break;
+        }
+    }
+
+    // Process
+    auto block = juce::dsp::AudioBlock<float>(buffer);
+    auto context = juce::dsp::ProcessContextReplacing<float>(block);
+
+    for (size_t i = 0; i < dspPointers.size(); ++i) {
+        if (dspPointers[i] != nullptr) {
+            dspPointers[i]->process(context);
+        }
+    }
+
     // This is the place where you'd normally do the guts of your plugin's
     // audio processing...
     // Make sure to reset the state if your inner loop is processing
